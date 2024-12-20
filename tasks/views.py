a397ff7e6b3358d49e90cc40722876823186b732
DiegoHrz <hernandezwdiego@gmail.com>
2024-12-19 19:54:21 -0500

    Add route Get and Post

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+#libreria para guardar una cookie con la info del usuario logeado
+from django.contrib.auth import login
+
+from django.db import IntegrityError
 
 # Create your views here.
-def hello_world(request):
+def home(request):
     # return HttpResponse('Hello world')
-    return HttpResponse('<h1>Hello world</h1>')
+    # return HttpResponse('<h1>Hello world</h1>')
+    # en lugar de retornar un string, es mejor devolver una pagina entera de html
+    # RETORNAR UN PAGINA HTML:
+    # En la carpeta del proyecto que estes puedes crear una carpeta llamada templates y dentro crea un archivo html 
+    # title = 'Hello worldcito'
+    return render(request, 'home.html')
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html',{
+            'form':UserCreationForm
+        })
+    else:
+        #comprobamos que la contraseña y la confirmacion de la contraseña sean iguales
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                #guardamos a todo el nuevo usuario creado una variable
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                # lo guardamos en la base de datos
+                user.save()
+                login(request,user)
+                return redirect('tasks')
+
+            #es el error cuando ya existe el user
+            except IntegrityError:
+                return HttpResponse(render(request,'signup.html',{
+                    'form': UserCreationForm,
+                    'error': 'Username already exists',
+                }))
+
+        return HttpResponse(render(request,'signup.html',{
+                    'form': UserCreationForm,
+                    'error': "Passwords dont match",
+                }))
+    
+def tasks(request):
+    return render(request,'tasks.html')
