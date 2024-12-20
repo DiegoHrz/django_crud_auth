098107414f4989c360aa609cc96601a7d838e54c
DiegoHrz <hernandezwdiego@gmail.com>
2024-12-19 23:42:55 -0500

    Add Task model

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
+
+#para añadir todos los atributos del modelo en el panel incluso los que no veo al inicio porque ya tienen valor x defecto
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created",)
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
