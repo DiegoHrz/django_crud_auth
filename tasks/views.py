204a12a01f5d008d5df18d9d9455b5a25ab1cea5
DiegoHrz <hernandezwdiego@gmail.com>
2024-12-23 10:27:33 -0500

    Add actualizar tarea

--- tasks/views.py
+++ tasks/views.py
@@ -63,10 +63,21 @@ def tasks(request):
 def task_detail(request, task_id):
     #esto lleva a error cuando en el url ponen un id inexistente
     # task = Task.objects.get(pk=task_id)
-    task = get_object_or_404(Task, pk=task_id)
-    return render(request,'task_detail.html',{
-        'task':task
-    })
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request,'task_detail.html',{
+            'task':task,            
+            'form': form
+        })
+    else:
+        try:
+            task = get_object_or_404(Task,pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task) 
+            form.save()
+            return redirect('tasks')
+        except ValueError :
+            return(render(request, 'task_detail.html',{'task': task,'form':form, 'error':'Error updating task' }))
 
 def sign_out(request):
     logout(request)
